f9f21445267365a5f563c07768e4c2995db2b5c1
mong_head <camara1melon315@gmail.com>
2021-06-03 12:19:18 +0900

    guestbook03 설정

---
+++ guestbook03/src/main/java/com/douzone/guestbook/respository/GuestbookRepository.java
@@ -0,0 +1,219 @@
+package com.douzone.guestbook.respository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.douzone.guestbook.vo.GuestbookVo;
+
+public class GuestbookRepository {
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			// 1. JDBC driver loading
+			// 		loading 후 Driver Class가 DriverManager만듦 - 우리가 new로 drivermanager만들 필요없음
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// 2. DriverManager - get connection
+			String url = "jdbc:mysql://192.168.254.40:3307/webdb?characterEncoding=utf8"; //linux server ip
+			conn	   = DriverManager.getConnection(url,"webdb","webdb");
+			conn.createStatement();
+			System.out.println("success connection");
+		} catch (ClassNotFoundException e) {
+			System.out.println("fail to load driver:"+e);
+		}
+		return conn;	
+
+	}
+	
+	public boolean insert(GuestbookVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		try {
+			conn = getConnection();
+			
+			// 3. prepare Statement 
+			String sql = "insert into guestbook values( null,?,?,?,now());";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. SQL binging
+			pstmt.setString(1, vo.getName()); // parameter mapping
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getMessage());
+			
+			// 5. SQL execute
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+			
+		} catch (SQLException e) {
+			// 2. 관련 : linux 꺼져있을 때 등등 connection안될 때 
+			System.out.println("error :"+e);
+		} finally {
+			//clean-up; 자원정리는 만들어진 순서 거꾸로 하기
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();					
+				}
+				
+			} catch (SQLException e) {
+				System.out.println("connection close error:"+e);
+			}
+		}
+		return result;
+	}
+
+	public List<GuestbookVo> findAll() {
+		List<GuestbookVo> result = new ArrayList<>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// 1,2 : driver loading, connection
+			conn = getConnection();
+			
+			// 3. prepare sql statement
+			String sql = "select no,name,password,message,reg_date"
+					+ " from guestbook"
+					+ " order by no desc";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. SQL실행 ( no binding )
+			rs = pstmt.executeQuery();
+			
+			// 6. result 가져오기
+			while(rs.next() /*각각의 행 가지고 오기*/) {
+				long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String password = rs.getString(3);
+				String message = rs.getString(4);
+				String reg_date = rs.getString(5);
+				
+				// mapping
+				GuestbookVo vo = new GuestbookVo();
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setPassword(password);
+				vo.setMessage(message);
+				vo.setRegDate(reg_date);
+				
+				result.add(vo);
+			}
+			
+		} catch (SQLException e) {
+			// 2. 관련 : linux 꺼져있을 때 등등 connection안될 때 
+			System.out.println("error :"+e);
+		} finally {
+			//clean-up; 자원정리는 만들어진 순서 거꾸로 하기
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();					
+				}
+				
+			} catch (SQLException e) {
+				System.out.println("connection close error:"+e);
+			}
+		}
+		return result;
+	}
+	public boolean delete(GuestbookVo vo){
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		try {
+			// 1,2 : driver loading, connection
+			conn = getConnection();
+			
+			// 3. Statement 생성 - query 실행위함
+			String sql = "delete from guestbook where no = ? and password = ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			// 4. binding
+			pstmt.setLong(1, vo.getNo());
+			pstmt.setString(2, vo.getPassword());
+			
+			// 5. SQL실행
+			int count = pstmt.executeUpdate();
+			result = count == 1;
+			
+		} catch (SQLException e) {
+			// 2. 관련 : linux 꺼져있을 때 등등 connection안될 때 
+			System.out.println("error :"+e);
+		} finally {
+			//clean-up; 자원정리는 만들어진 순서 거꾸로 하기
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();					
+				}
+				
+			} catch (SQLException e) {
+				System.out.println("connection close error:"+e);
+			}
+		}
+		return result;
+	}
+	public String findPassword(GuestbookVo vo) {
+		String result = null;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// 1,2 : driver loading, connection
+			conn = getConnection();
+			
+			// 3. prepare sql statement
+			String sql = "select password"
+					+ " from guestbook"
+					+ " where no = ?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setLong(1, vo.getNo());
+			
+			// 4. SQL실행 
+			rs = pstmt.executeQuery();
+			
+			// 6. result 가져오기
+			while(rs.next() /*각각의 행 가지고 오기*/) {
+				result = rs.getString(1);
+			}
+			
+		} catch (SQLException e) {
+			// 2. 관련 : linux 꺼져있을 때 등등 connection안될 때 
+			System.out.println("error :"+e);
+		} finally {
+			//clean-up; 자원정리는 만들어진 순서 거꾸로 하기
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if(conn != null) {
+					conn.close();					
+				}
+				
+			} catch (SQLException e) {
+				System.out.println("connection close error:"+e);
+			}
+		}
+		return result;
+	}
+}
---
+++ guestbook03/src/main/java/com/douzone/guestbook/vo/GuestbookVo.java
@@ -0,0 +1,48 @@
+package com.douzone.guestbook.vo;
+
+public class GuestbookVo {
+	private long no;
+	private String name;
+	private String password;
+	private String regDate;
+	private String message;
+	
+	public long getNo() {
+		return no;
+	}
+	public void setNo(long no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return "GuestbookVo [no=" + no + ", name=" + name + ", password=" + password + ", regDate=" + regDate
+				+ ", message=" + message + "]";
+	}
+	
+	
+}
